d10e4c788e920bc46618341959a307ca0db4ca3d
khacthe <khacthe.python@gmail.com>
2017-05-16 08:02:13 +0700

    Search device

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -2,15 +2,15 @@ class DevicesController < ApplicationController
 
   def index
     if params[:category_id].present?
-      @devices = Device.get_devices_by_category(category_id)
-        .page(params[:page]).per Settings.view.device.per_page
+      load_devices_by_category params[:category_id]
     else
-      @devices = Device.get_all_devices
+      @search = Device.search(params[:q])
+      @devices = @search.result
         .page(params[:page]).per Settings.view.device.per_page
+      return if @devices.present?
+      flash[:errors] = t "device.device_not_found"
+      redirect_to devices_path
     end
-    return if @devices.present?
-    flash[:errors] = t "device.device_not_found"
-    redirect_to root_path
   end
 
   def show
